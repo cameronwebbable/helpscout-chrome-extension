8c103aef521bfcedf465bf1de02c8b22d0245655
Cameron Webb <cwebb02@gmail.com>
2014-02-11 10:59:15 -0500

    code for teh masses

---
+++ background.js
@@ -0,0 +1,186 @@
+var helpScoutAPIURL = "https://api.helpscout.net/v1/mailboxes/<MAILBOX ID HERE>/conversations.json?status=active";
+var helpScoutDashboardURL = "https://secure.helpscout.net/dashboard/";
+var helpScoutAPIKey = "<API KEY HERE>";
+var pollIntervalMin = 1;  // 1 minute
+var pollIntervalMax = 60;  // 1 hour
+var requestTimeout = 1000 * 2;
+var oldChromeVersion = !chrome.runtime;
+
+function getMailboxAPI()
+{
+	return helpScoutAPIURL;
+}
+
+function make_base_auth(user, password) {
+  var tok = user + ':' + password;
+  var hash = btoa(tok);
+  return "Basic " + hash;
+}
+
+function onInit() {
+  console.log('onInit');
+  localStorage.requestFailureCount = 0;  // used for exponential backoff
+  startRequest({scheduleRequest:true, showLoadingAnimation:true});
+  if (!oldChromeVersion)  {
+    // TODO(mpcomplete): We should be able to remove this now, but leaving it
+    // for a little while just to be sure the refresh alarm is working nicely.
+    chrome.alarms.create('watchdog', {periodInMinutes:5});
+  }
+}
+
+function onAlarm(alarm) {
+  console.log('Got alarm', alarm);
+  // |alarm| can be undefined because onAlarm also gets called from
+  // window.setTimeout on old chrome versions.
+  if (alarm && alarm.name == 'watchdog') {
+    onWatchdog();
+  } else {
+    startRequest({scheduleRequest:true, showLoadingAnimation:false});
+  }
+}
+function updateIcon() {
+  if (!localStorage.hasOwnProperty('unreadCount')) {
+    chrome.browserAction.setBadgeText({text:""});
+  } else {
+    chrome.browserAction.setBadgeBackgroundColor({color:[208, 0, 24, 255]});
+    chrome.browserAction.setBadgeText({
+      text: localStorage.unreadCount != "0" ? localStorage.unreadCount : ""
+    });
+  }
+}
+function scheduleRequest() {
+  console.log('scheduleRequest');
+  var randomness = Math.random() * 2;
+  var exponent = Math.pow(2, localStorage.requestFailureCount || 0);
+  var multiplier = Math.max(randomness * exponent, 1);
+  var delay = Math.min(multiplier * pollIntervalMin, pollIntervalMax);
+  delay = Math.round(delay);
+  console.log('Scheduling for: ' + delay);
+
+  if (oldChromeVersion) {
+    if (requestTimerId) {
+      window.clearTimeout(requestTimerId);
+    }
+    requestTimerId = window.setTimeout(onAlarm, delay*60*1000);
+  } else {
+    console.log('Creating alarm');
+    // Use a repeating alarm so that it fires again if there was a problem
+    // setting the next alarm.
+    chrome.alarms.create('refresh', {periodInMinutes: delay});
+  }
+}
+
+function getInboxCount(onSuccess, onError) {
+  var xhr = new XMLHttpRequest();
+  var abortTimerId = window.setTimeout(function() {
+    xhr.abort();  // synchronously calls onreadystatechange
+  }, requestTimeout);
+
+  function handleSuccess(count) {
+    localStorage.requestFailureCount = 0;
+    window.clearTimeout(abortTimerId);
+    if (onSuccess)
+      onSuccess(count);
+  }
+
+  var invokedErrorCallback = false;
+  function handleError() {
+    ++localStorage.requestFailureCount;
+    window.clearTimeout(abortTimerId);
+    if (onError && !invokedErrorCallback)
+      onError();
+    invokedErrorCallback = true;
+  }
+
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState != 4)
+        return;
+	  
+		var data = JSON.parse(xhr.responseText);
+        var inboxItems = data["items"];
+		var count = 0;
+		console.log(inboxItems);
+		inboxItems.forEach(function(entry)
+		{
+			if (!entry["owner"])
+			{
+				count++;
+			}
+		});
+		onSuccess(count);
+    };
+
+    xhr.onerror = function(error) {
+      handleError();
+    };
+
+    xhr.open("GET", getMailboxAPI(), true);
+    xhr.setRequestHeader("Authorization", make_base_auth(helpScoutAPIKey, "X"));
+    xhr.send(null);
+}
+
+function updateUnreadCount(count) {
+  localStorage.unreadCount = count;
+  updateIcon();
+}
+function startRequest(params) {
+  // Schedule request immediately. We want to be sure to reschedule, even in the
+  // case where the extension process shuts down while this request is
+  // outstanding.
+  if (params && params.scheduleRequest) scheduleRequest();
+
+  getInboxCount(
+    function(count) {
+      updateUnreadCount(count);
+    },
+    function() {
+      delete localStorage.unreadCount;
+      updateIcon();
+    }
+  );
+}
+
+function onWatchdog() {
+  chrome.alarms.get('refresh', function(alarm) {
+    if (alarm) {
+      console.log('Refresh alarm exists. Yay.');
+    } else {
+      console.log('Refresh alarm doesn\'t exist!? ' +
+                  'Refreshing now and rescheduling.');
+      startRequest({scheduleRequest:true, showLoadingAnimation:false});
+    }
+  });
+}
+
+function goToInbox() {
+  console.log('Going to support inbox...');
+  chrome.tabs.getAllInWindow(undefined, function(tabs) {
+    chrome.tabs.create({url: helpScoutDashboardURL});
+  });
+}
+if (oldChromeVersion) {
+  updateIcon();
+  onInit();
+} else {
+  chrome.runtime.onInstalled.addListener(onInit);
+  chrome.alarms.onAlarm.addListener(onAlarm);
+}
+
+chrome.browserAction.onClicked.addListener(goToInbox);
+if (chrome.runtime && chrome.runtime.onStartup) {
+  chrome.runtime.onStartup.addListener(function() {
+    console.log('Starting browser... updating icon.');
+    startRequest({scheduleRequest:false, showLoadingAnimation:false});
+    updateIcon();
+  });
+} else {
+  // This hack is needed because Chrome 22 does not persist browserAction icon
+  // state, and also doesn't expose onStartup. So the icon always starts out in
+  // wrong state. We don't actually use onStartup except as a clue that we're
+  // in a version of Chrome that has this problem.
+  chrome.windows.onCreated.addListener(function() {
+    console.log('Window created... updating icon.');
+    startRequest({scheduleRequest:false, showLoadingAnimation:false});
+    updateIcon();
+  });
+}
